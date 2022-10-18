3f8d53bbbbadd6567b0a464eb82ea078d4e41416
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-10-18 19:16:22 +0100

    Quick initial Recipes component

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { useState } from "react";
+import "./Recipes.css";
 import Navigation from "./Navigation";
+import Button from "react-bootstrap/esm/Button";
 
 export default function Recipes() {
+  const [smoothieName, setSmoothieName] = useState("");
+  const [smoothieFruits, setSmoothieFruits] = useState("");
+
+  let tropical = ["pineapple", "mango", "banana"];
+
+  function showSmoothie(event) {
+    setSmoothieName(event.target.innerHTML);
+    let ingredients = tropical.map((ingredient, index) => {
+      return <li key={index}>{ingredient}</li>;
+    });
+    setSmoothieFruits(ingredients);
+  }
+
   return (
     <div className="Recipes">
       <Navigation />
       <div className="container">
         <h1>Recipes</h1>
+        <div className="section smoothie-bar">
+          <Button onClick={showSmoothie}>Tropical</Button>
+
+          <Button onClick={showSmoothie}>Smoothie 2</Button>
+
+          <Button onClick={showSmoothie}>Smoothie 3</Button>
+
+          <Button onClick={showSmoothie}>Smoothie 4</Button>
+
+          <Button onClick={showSmoothie}>Smoothie 5</Button>
+
+          <Button onClick={showSmoothie}>Smoothie 6</Button>
+        </div>
+      </div>
+
+      <hr></hr>
+      <div className="container">
+        <h1>{smoothieName}</h1>
+        <ul>{smoothieFruits}</ul>
       </div>
     </div>
   );
