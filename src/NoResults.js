635fce013bac278f2fb4a340c87170d575fb3331
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-11-09 23:33:41 +0000

    Added favicon and footer logo, styled singleFruit listings to match other cards

---
+++ src/NoResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NoResults.css";
+
+export default function NoResults() {
+  return (
+    <div className="NoResults">
+      <p>
+        <strong>No results for your search.</strong>
+        <br></br> Please try searching for another fruit!
+      </p>
+    </div>
+  );
+}
