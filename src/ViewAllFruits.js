7d173d1061ee55aaeda52bfd6dbbaed92b9cab56
Muhammad Faizan <m.faizen@outlook.com>
2022-11-12 22:23:43 +0000

    Update ViewAllFruits.js

--- src/ViewAllFruits.js
+++ src/ViewAllFruits.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import FruitApi from "./FruitApi";
 import { useState } from "react";
+import MultipleFruits from "./MultipleFruits";
+import Dropdowns from "./Dropdowns";
+import Sort from "./Sort";
+import Listing from "./Listing";
 
-export default function ViewAllFruits() {
+export default function ViewAllFruits({ singleFruitUrl, url, searchToFruit }) {
   const [showFruit, setShowFruits] = useState(false);
+  const [sortBy, setSortBy] = useState("Name");
 
   function allFruits() {
+    searchToFruit("");
     setShowFruits(true);
     setShowRandomFruit(0);
   }
@@ -14,6 +19,7 @@ export default function ViewAllFruits() {
 
   function randomUrl() {
     setShowRandomFruit(showRandomFruit + 1);
+    searchToFruit("");
     setShowFruits(false);
   }
 
@@ -23,9 +29,25 @@ export default function ViewAllFruits() {
         <Button onClick={allFruits}>View all fruits</Button>
         <Button onClick={randomUrl}>Random fruit</Button>
       </div>
-      {(showFruit || showRandomFruit > 0) && (
-        <FruitApi showRandomFruit={showRandomFruit} />
-      )}
+      <div className="main">
+        <div className="left-main">
+          <Dropdowns />
+        </div>
+        {(showFruit || showRandomFruit > 0) && (
+          <MultipleFruits
+            showRandomFruit={showRandomFruit}
+            url={url}
+            singleFruitUrl={singleFruitUrl}
+            sortBy={sortBy.toLowerCase()}
+          />
+        )}
+        <div className="right-main">
+          <div className="sort-section">
+            <Sort sortBy={sortBy} setSortBy={setSortBy} />
+          </div>
+          <Listing />
+        </div>
+      </div>
     </div>
   );
 }
