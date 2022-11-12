b17ad87b116ffcfa254453f3f2bd9d7ee64f6a5a
Muhammad Faizan <m.faizen@outlook.com>
2022-11-12 22:27:12 +0000

    Revert "Update ViewAllFruits.js"
    This reverts commit 7d173d1061ee55aaeda52bfd6dbbaed92b9cab56.

--- src/ViewAllFruits.js
+++ src/ViewAllFruits.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import FruitApi from "./FruitApi";
 import { useState } from "react";
-import MultipleFruits from "./MultipleFruits";
-import Dropdowns from "./Dropdowns";
-import Sort from "./Sort";
-import Listing from "./Listing";
 
-export default function ViewAllFruits({ singleFruitUrl, url, searchToFruit }) {
+export default function ViewAllFruits() {
   const [showFruit, setShowFruits] = useState(false);
-  const [sortBy, setSortBy] = useState("Name");
 
   function allFruits() {
-    searchToFruit("");
     setShowFruits(true);
     setShowRandomFruit(0);
   }
@@ -19,7 +14,6 @@ export default function ViewAllFruits({ singleFruitUrl, url, searchToFruit }) {
 
   function randomUrl() {
     setShowRandomFruit(showRandomFruit + 1);
-    searchToFruit("");
     setShowFruits(false);
   }
 
@@ -29,25 +23,9 @@ export default function ViewAllFruits({ singleFruitUrl, url, searchToFruit }) {
         <Button onClick={allFruits}>View all fruits</Button>
         <Button onClick={randomUrl}>Random fruit</Button>
       </div>
-      <div className="main">
-        <div className="left-main">
-          <Dropdowns />
-        </div>
-        {(showFruit || showRandomFruit > 0) && (
-          <MultipleFruits
-            showRandomFruit={showRandomFruit}
-            url={url}
-            singleFruitUrl={singleFruitUrl}
-            sortBy={sortBy.toLowerCase()}
-          />
-        )}
-        <div className="right-main">
-          <div className="sort-section">
-            <Sort sortBy={sortBy} setSortBy={setSortBy} />
-          </div>
-          <Listing />
-        </div>
-      </div>
+      {(showFruit || showRandomFruit > 0) && (
+        <FruitApi showRandomFruit={showRandomFruit} />
+      )}
     </div>
   );
 }
